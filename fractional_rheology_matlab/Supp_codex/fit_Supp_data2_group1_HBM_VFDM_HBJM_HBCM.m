464369c0d2b483d25e4bb3e46c720d568fdccbff
yanshengjie244 <Yansj990909@163.com>
2026-05-21 16:31:33 +0800

    Update fit_Supp_data2_group1_HBM_VFDM_HBJM_HBCM.m

--- fractional_rheology_matlab/Supp_codex/fit_Supp_data2_group1_HBM_VFDM_HBJM_HBCM.m
+++ fractional_rheology_matlab/Supp_codex/fit_Supp_data2_group1_HBM_VFDM_HBJM_HBCM.m
@@ -1,4 +1,4 @@
 %% fit_data2_group1_HBM_VFDM_HBJM_HBCM.m
 % Data set 2, Group 1: columns 1-2.
 
-run_group_HBM_VFDM_HBJM_HBCM(1, char([27969 21464 25968 25454 50 46 120 108 115 120]));
+run_group_HBM_VFDM_HBJM_HBCM(1, 'Supp_data_2.xlsx');
